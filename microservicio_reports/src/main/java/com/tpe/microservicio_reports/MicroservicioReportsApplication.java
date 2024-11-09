9123ca11c001d8629a1e1e03baf5b83da8490035
Luciano <oroquietaluciano@gmail.com>
2024-11-09 20:13:29 -0300

    config properties.yml and dependencies for entities

--- microservicio_reports/src/main/java/com/tpe/microservicio_reports/MicroservicioReportsApplication.java
+++ microservicio_reports/src/main/java/com/tpe/microservicio_reports/MicroservicioReportsApplication.java
@@ -2,8 +2,10 @@ package com.tpe.microservicio_reports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicioReportsApplication {
 
     public static void main(String[] args) {
--- microservicio_users/src/main/java/com/tpe/microservicio_users/MicroservicioUsersApplication.java
+++ microservicio_users/src/main/java/com/tpe/microservicio_users/MicroservicioUsersApplication.java
@@ -2,8 +2,10 @@ package com.tpe.microservicio_users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicioUsersApplication {
 
     public static void main(String[] args) {
