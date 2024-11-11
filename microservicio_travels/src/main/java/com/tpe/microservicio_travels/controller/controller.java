c58001fd27a9cc1459e9b44bc064df6c2a08fb76
Luciano <oroquietaluciano@gmail.com>
2024-11-11 15:42:41 -0300

    test

--- microservicio_travels/src/main/java/com/tpe/microservicio_travels/controller/controller.java
+++ microservicio_travels/src/main/java/com/tpe/microservicio_travels/controller/controller.java
@@ -1,4 +1,15 @@
 package com.tpe.microservicio_travels.controller;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/test")
 public class controller {
+
+    @GetMapping("")
+    public String test(){
+        return "Test";
+    }
+
 }
