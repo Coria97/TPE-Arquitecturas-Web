d006569cca1dd535831b7cda5c2b2639085fbceb
Coria97 <santiagocoria@live.com.ar>
2024-11-15 02:40:13 -0300

    refactor finish travel

--- microservicio_travels/src/main/java/com/tpe/microservicio_travels/dto/ScooterStatsUpdateDTO.java
+++ microservicio_travels/src/main/java/com/tpe/microservicio_travels/dto/ScooterStatsUpdateDTO.java
@@ -1,11 +1,18 @@
 package com.tpe.microservicio_travels.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+
 public class ScooterStatsUpdateDTO {
+    @JsonProperty("km")
     private float km;
+    @JsonProperty("timeUsage")
     private float timeUsage;
+
+    public ScooterStatsUpdateDTO() {}
 
     public ScooterStatsUpdateDTO(float km, float timeUsage) {
         this.km = km;
--- microservicio_travels/src/main/java/com/tpe/microservicio_travels/feign/ScooterFeignClient.java
+++ microservicio_travels/src/main/java/com/tpe/microservicio_travels/feign/ScooterFeignClient.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name="microservicio-users",  url = "http://localhost:8081")
+@FeignClient(name="microservicio-stops",  url = "http://localhost:8081")
 public interface ScooterFeignClient {
     @PutMapping("/api/scooter/{scooterId}/stats")
-    void updateStatsScooter(@PathVariable Long id, @RequestBody ScooterStatsUpdateDTO scooterUpdateDTO);
+    void updateStatsScooter(@PathVariable("scooterId") Long scooterId, @RequestBody ScooterStatsUpdateDTO scooterUpdateDTO);
+
 }
